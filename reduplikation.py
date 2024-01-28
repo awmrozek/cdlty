63d15fc24a2e5418375665ee0793a8d946177eff
welw <mrozekss@gmail.com>
2024-01-29 00:01:50 +0100

    Task reduplication

---
+++ reduplikation.py
@@ -0,0 +1,18 @@
+from collections import *
+import sys
+itr = (line for line in sys.stdin.read().split())
+INP = lambda: next(itr)
+
+# Returns next integer from STDIN
+def ni(): return int(INP())
+def nf(): return float(INP())
+
+# Returns next line
+def nl(): return [_ for _ in INP().split()]
+
+word=''.join(nl())
+times=ni()
+
+for x in range(0, times):
+	print(word, end="")
+print("")
