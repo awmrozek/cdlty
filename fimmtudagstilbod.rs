b339e45680ef5401063366bbf966c0bc77d0e0c9
welw <mrozekss@gmail.com>
2024-08-05 00:48:50 +0200

    Calculate function y = 1000 + 100 * (x-2020)

---
+++ fimmtudagstilbod.rs
@@ -0,0 +1,18 @@
+// Calculate function y = 1000 + 100 * (x-2020)
+use std::io::{self, Write};
+
+fn main() {
+    // Prompt the user to enter an integer x
+    io::stdout().flush().unwrap();
+
+    // Read the input integer x
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Failed to read line");
+    let x: i32 = input.trim().parse().expect("Please enter a valid integer");
+
+    // Calculate y = 2020 + 100 * x
+    let y = 1000 + 100 * (x-2020);
+
+    // Print the result
+    println!("{}", y);
+}
